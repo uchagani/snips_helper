f171ab0958f163637312bb64a3371b62a51d1525
Tod Schmidt <tschmidty@yahoo.com>
2018-03-27 09:45:57 -0400

    Much change

--- snips_helper/bundle.py
+++ snips_helper/bundle.py
@@ -2,10 +2,11 @@ from .intent import Intent
 
 
 class Bundle:
-    def __init__(self, name, driver):
-        print("Creating bundle: {}".format(name))
+    def __init__(self, name, driver, debug):
+        if debug: print("Creating bundle: {}".format(name))
         self.__name = name
         self.__driver = driver
+        self.__debug = debug
 
     @property
     def name(self):
@@ -22,17 +23,17 @@ class Bundle:
         return self.driver.find_element_by_xpath(selector)
 
     def __activate(self):
-        print("Selecting bundle: {}".format(self.name))
+        if self.__debug: print("Selecting bundle: {}".format(self.name))
         self.__get_bundle().click()
 
     def get_intents(self):
-        print("Getting intents for: {}".format(self.name))
+        if self.__debug: print("Getting intents for: {}".format(self.name))
         self.__activate()
         intent_elements = self.driver.find_elements_by_class_name(
             'assistant-intent__title')
 
         intents = []
         for intent in intent_elements:
-            intents.append(Intent(intent.text, self.driver))
+            intents.append(Intent(intent.text, self.driver, self.__debug))
 
         return intents
