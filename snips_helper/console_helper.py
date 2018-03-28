5e57e8afe03b8978b1e4f15f6a742063e5cb40e3
uchagani <umair.chagani@manheim.com>
2018-03-27 23:12:27 -0400

    change print statements to a proper logger

--- snips_helper/console_helper.py
+++ snips_helper/console_helper.py
@@ -5,6 +5,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 import os
 from .bundle import Bundle
+import logging
 
 
 class ConsoleHelper:
@@ -12,11 +13,22 @@ class ConsoleHelper:
     IMPLICIT_DELAY = 10
 
     def __init__(self, chrome_driver_path='/usr/bin/chromedriver',
-                 download_dir=None, headless=True, debug=False):
+                 download_dir=None, headless=True, log_level='WARNING'):
+
+        self.logger = logging.getLogger('snips_helper')
+
+        numeric_level = getattr(logging, log_level.upper(), None)
+        if not isinstance(numeric_level, int):
+            raise ValueError('Invalid log level: %s' % log_level)
+
+        self.logger.setLevel(level=numeric_level)
+        ch = logging.StreamHandler()
+        ch.setLevel(numeric_level)
+        self.logger.addHandler(ch)
+        
         if download_dir is None:
             download_dir = os.getcwd()
 
-        self.debug = debug
         self.chrome_options = Options()
         if headless:
             self.chrome_options.add_argument("--headless")
@@ -29,11 +41,11 @@ class ConsoleHelper:
             "safebrowsing.enabled": True
         })
 
-        if self.debug: print("Creating chrome driver")
+        self.logger.debug("Creating chrome driver")
         self.driver = webdriver.Chrome(chrome_options=self.chrome_options,
                                        executable_path=chrome_driver_path)
 
-        if self.debug: print("Sending command to enable headless downloads")
+        self.logger.debug("Sending command to enable headless downloads")
         self.driver.command_executor._commands["send_command"] = (
             "POST", '/session/$sessionId/chromium/send_command')
         params = {'cmd': 'Page.setDownloadBehavior',
@@ -43,24 +55,24 @@ class ConsoleHelper:
         self.driver.execute("send_command", params)
         self.driver.implicitly_wait(self.IMPLICIT_DELAY)
 
-        if self.debug: print("Setting window size to maximize")
+        self.logger.debug("Setting window size to maximize")
         self.driver.set_window_position(0,0)
         self.driver.set_window_size(4096, 2160)
         self.driver.maximize_window()
 
     def login(self, email, password, assistant=None):
-        if self.debug: print("Navigating to Snips Console")
+        self.logger.debug("Navigating to Snips Console")
         self.driver.get(self.LOGIN_URL)
         self.handle_cookie_message()
-        if self.debug: print("Getting email field")
+        self.logger.debug("Getting email field")
         email_field = self.driver.find_element_by_name('email')
-        if self.debug: print("Setting email field")
+        self.logger.debug("Setting email field")
         email_field.send_keys(email)
-        if self.debug: print("Getting password field")
+        self.logger.debug("Getting password field")
         password_field = self.driver.find_element_by_name('password')
-        if self.debug: print("Setting password field")
+        self.logger.debug("Setting password field")
         password_field.send_keys(password)
-        if self.debug: print("Submitting login")
+        self.logger.debug("Submitting login")
         password_field.submit()
 
         if assistant is not None:
@@ -70,7 +82,7 @@ class ConsoleHelper:
         self.driver.quit()
 
     def change_assistant(self, assistant):
-        if self.debug: print("Changing assistant")
+        self.logger.debug("Changing assistant")
         assistant_selector = self.driver.find_element_by_class_name(
             "header-assistants-select")
         assistant_selector.click()
@@ -82,7 +94,7 @@ class ConsoleHelper:
         assistant.click()
 
     def get_assistant(self):
-        if self.debug: print("Getting assistant")
+        self.logger.debug("Getting assistant")
         assistant_label = self.driver.find_element_by_class_name(
             "header-assistants-select__label").text
         return assistant_label
@@ -91,7 +103,7 @@ class ConsoleHelper:
         try:
             cookies_button = self.driver.find_element_by_class_name(
                 'cookies-usage-info__ok-button')
-            if self.debug: print("Handling cookie message")
+            self.logger.debug("Handling cookie message")
             cookies_button.click()
         except:
             return
@@ -101,29 +113,29 @@ class ConsoleHelper:
             'header-assistants-select__label')
 
     def get_bundles(self):
-        if self.debug: print("Changing bundles")
+        self.logger.debug("Changing bundles")
         bundle_elements = self.driver.find_elements_by_class_name(
             'bundle-header__title')
         bundles = []
         for bundle_element in bundle_elements:
-            bundles.append(Bundle(bundle_element.text, self.driver, self.debug))
+            bundles.append(Bundle(bundle_element.text, self.driver))
 
         return bundles
 
     def retrain_assistant(self):
-        if self.debug: print("Retraining assistant")
+        self.logger.debug("Retraining assistant")
         self.get_header().click()
         bundle = self.get_bundles()[0]
         intent = bundle.get_intents()[0]
         intent.save()
 
     def get_download_button(self, timeout):
-        if self.debug: print("Getting download button")
+        self.logger.debug("Getting download button")
         return WebDriverWait(self.driver, timeout).until(
             EC.element_to_be_clickable((By.CSS_SELECTOR, "[download]")))
 
     def get_download_close_button(self, timeout):
-        if self.debug: print("Getting download close button")
+        self.logger.debug("Getting download close button")
         download_modal = WebDriverWait(self.driver, timeout).until(
             EC.presence_of_element_located(
                 (By.CLASS_NAME, "download-assistant-modal")))
@@ -133,6 +145,6 @@ class ConsoleHelper:
                 (By.XPATH, "//*[text()='Close']")))
 
     def download_assistant(self, timeout=120):
-        if self.debug: print("Downloading assistant")
+        self.logger.debug("Downloading assistant")
         self.get_download_button(timeout).click()
         self.get_download_close_button(timeout).click()
--- snips_helper/intent.py
+++ snips_helper/intent.py
@@ -1,11 +1,12 @@
 import time
+import logging
 
 class Intent:
-    def __init__(self, name, driver, debug):
-        if debug: print("Creating intent: {}".format(name))
+    def __init__(self, name, driver):
+        self.logger = logging.getLogger('snips_helper')
+        self.logger.debug("Creating intent: {}".format(name))
         self.__name = name
         self.__driver = driver
-        self.__debug = debug
 
     @property
     def name(self):
@@ -16,7 +17,7 @@ class Intent:
         return self.__driver
 
     def utterances(self):
-        if self.__debug: print("Getting utterances")
+        self.logger.debug("Getting utterances")
         self.__activate()
         self.__get_utterance_dot_button().click()
         self.__get_export_utterance_button().click()
@@ -26,7 +27,7 @@ class Intent:
         return training_data
 
     def slots(self):
-        if self.__debug: print("Getting slots")
+        self.logger.debug("Getting slots")
         self.__activate()
         self.__get_slots_dot_button().click()
         self.__get_export_slots_button().click()
@@ -37,12 +38,12 @@ class Intent:
         return slots
 
     def export_slot_values(self, slot):
-        if self.__debug: print("Getting slot: {}".format(slot))
+        self.logger.debug("Getting slot: {}".format(slot))
         self.__activate()
         if self.__get_slot_editor_button(slot):
             self.__get_slot_editor_button(slot).click()
         else:
-            if self.__debug: print("No slots found {}".format(self.__get_slot_editor_button(slot)))
+            self.logger.debug("No slots found {}".format(self.__get_slot_editor_button(slot)))
             self.__deactivate()
             return None
         self.__get_slot_modal_export_button().click()
@@ -53,7 +54,7 @@ class Intent:
         return slot_values
 
     def import_utterances(self, utterances):
-        if self.__debug: print("Importing utterances")
+        self.logger.debug("Importing utterances")
         self.__activate()
         self.__get_utterance_dot_button().click()
         self.__get_import_utterance_button().click()
@@ -64,7 +65,7 @@ class Intent:
         return
 
     def delete_utterances(self):
-        if self.__debug: print("Deleting utterances")
+        self.logger.debug("Deleting utterances")
         self.__activate()
         while self.__get_utterance_delete_button():
             self.__get_utterance_delete_button().click()
@@ -72,7 +73,7 @@ class Intent:
         return
 
     def save(self):
-        if self.__debug: print("Saving intent: {}".format(self.name))
+        self.logger.debug("Saving intent: {}".format(self.name))
         if not self.__get_save_button():
             self.__activate()
         self.__get_save_button().click()
@@ -80,93 +81,93 @@ class Intent:
         self.__deactivate()
 
     def __activate(self):
-        if self.__debug: print("Activating intent: {}".format(self.name))
+        self.logger.debug("Activating intent: {}".format(self.name))
         self.__get_intent().click()
 
     def __deactivate(self):
-        if self.__debug: print("Deactivating intent: {}".format(self.name))
+        self.logger.debug("Deactivating intent: {}".format(self.name))
         self.__get_back_to_assistant_button().click()
 
     def __get_intent(self):
-        if self.__debug: print("Getting intent: {}".format(self.name))
+        self.logger.debug("Getting intent: {}".format(self.name))
         selector1 = "//*[contains(@class, 'assistant-intent__title')]"
         selector2 = "[text()='{}']".format(self.name)
         selector = selector1 + selector2
         return self.driver.find_element_by_xpath(selector)
 
     def __get_back_to_assistant_button(self):
-        if self.__debug: print("Get back to assistant button")
+        self.logger.debug("Get back to assistant button")
         return self.driver.find_element_by_link_text('Back to Assistant')
 
     def __get_save_button(self):
-        if self.__debug: print("Get save button")
+        self.logger.debug("Get save button")
         return self.driver.find_element_by_class_name('save-intent-button')
 
     def __get_export_headers(self):
-        if self.__debug: print("Get export headers")
+        self.logger.debug("Get export headers")
         return self.driver.find_elements_by_class_name(
             'intent-editor-section-header')
 
     def __get_import_headers(self):
-        if self.__debug: print("Get import headers")
+        self.logger.debug("Get import headers")
         return self.driver.find_elements_by_class_name(
             'intent-editor-section-header')
 
     def __get_utterance_section(self):
-        if self.__debug: print("Get utterance section")
+        self.logger.debug("Get utterance section")
         return self.__get_export_headers()[1]
 
     def __get_utterance_delete_button(self):
-        if self.__debug: print("Get utterance delete button")
+        self.logger.debug("Get utterance delete button")
         selector = 'button--unstyled'
         utterance_section = self.__get_utterance_section()
         return utterance_section.find_element_by_class_name(selector)
 
     def __get_utterance_dot_button(self):
-        if self.__debug: print("Get utterance dot button")
+        self.logger.debug("Get utterance dot button")
         utterance_section = self.__get_utterance_section()
         return utterance_section.find_element_by_class_name('dots-icon-button')
 
     def __get_export_utterance_button(self):
-        if self.__debug: print("Get export utterance button")
+        self.logger.debug("Get export utterance button")
         selector = "//*[text()='Export Training Examples']"
         return self.driver.find_element_by_xpath(selector)
 
     def __get_import_utterance_button(self):
-        if self.__debug: print("Get import utterance button")
+        self.logger.debug("Get import utterance button")
         selector = "//*[text()='Import Training Examples']"
         return self.driver.find_element_by_xpath(selector)
 
     def __get_slots_section(self):
-        if self.__debug: print("Get slots section")
+        self.logger.debug("Get slots section")
         return self.__get_export_headers()[0]
 
     def __get_slots_dot_button(self):
-        if self.__debug: print("Get slots dot button")
+        self.logger.debug("Get slots dot button")
         slot_section = self.__get_slots_section()
         return slot_section.find_element_by_class_name('dots-icon-button')
 
     def __get_export_slots_button(self):
-        if self.__debug: print("Get export slots button")
+        self.logger.debug("Get export slots button")
         selector = "//*[text()='Export Slots']"
         return self.driver.find_element_by_xpath(selector)
 
     def __get_import_slots_button(self):
-        if self.__debug: print("Get import slots button")
+        self.logger.debug("Get import slots button")
         selector = "//*[text()='Import Slots']"
         return self.driver.find_element_by_xpath(selector)
 
     def __get_slots_island(self):
-        if self.__debug: print("Get slot island")
+        self.logger.debug("Get slot island")
         return self.driver.find_elements_by_class_name('island--with-row')[0]
 
     def __get_slot_editors(self):
-        if self.__debug: print("Get slots editors")
+        self.logger.debug("Get slots editors")
         slots_island = self.__get_slots_island()
         return slots_island.find_elements_by_class_name('slot-editor')
 
     def __get_slot_editor_row(self, slot):
-        if self.__debug: print("Get slot editor row")
+        self.logger.debug("Get slot editor row")
         for editor in self.__get_slot_editors():
             slot_name = editor.find_element_by_class_name(
                 'slot-editor__name').get_attribute('value')
@@ -174,7 +175,7 @@ class Intent:
                 return editor
 
     def __get_slot_editor_button(self, slot):
-        if self.__debug: print("Get slot editor button")
+        self.logger.debug("Get slot editor button")
         editor_row = self.__get_slot_editor_row(slot)
         if not editor_row:
             return None
@@ -189,17 +190,17 @@ class Intent:
         return editor_button
 
     def __get_slot_modal(self):
-        if self.__debug: print("Get slot modal")
+        self.logger.debug("Get slot modal")
         return self.driver.find_element_by_class_name(
             'fullscreen-modal')
 
     def __get_slot_modal_content(self):
-        if self.__debug: print("Get slot modal content")
+        self.logger.debug("Get slot modal content")
         modal = self.__get_slot_modal()
         return modal.find_element_by_class_name('fullscreen-modal__content')
 
     def __get_slot_modal_export_button(self):
-        if self.__debug: print("Get slot modal export button")
+        self.logger.debug("Get slot modal export button")
         modal_content = self.__get_slot_modal_content()
         export_button = modal_content.find_element_by_xpath("//*[text()=' Export']")
         self.driver.execute_script(
@@ -218,26 +219,26 @@ class Intent:
         return close_button
 
     def __get_modal(self):
-        if self.__debug: print("Get modal")
+        self.logger.debug("Get modal")
         return self.driver.find_element_by_class_name(
             'modal-dialog__dialog')
 
     def __get_modal_content(self):
-        if self.__debug: print("Get modal content")
+        self.logger.debug("Get modal content")
         modal = self.__get_modal()
         return modal.find_element_by_class_name('modal-dialog__content')
 
     def __get_modal_text_area(self):
-        if self.__debug: print("Get utterance text area")
+        self.logger.debug("Get utterance text area")
         modal = self.__get_modal_content()
         return modal.find_element_by_tag_name('textarea')
 
     def __get_modal_close_button(self):
-        if self.__debug: print("Get modal close button")
+        self.logger.debug("Get modal close button")
         modal = self.__get_modal()
         return modal.find_element_by_xpath("//*[text()='Close']")
 
     def __get_modal_import_button(self):
-        if self.__debug: print("Get modal import button")
+        self.logger.debug("Get modal import button")
         modal = self.__get_modal()
         return modal.find_element_by_xpath("//*[text()='Import']")
