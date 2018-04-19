e17d98434303a4bfebc5ca80464355a47791fdcf
uchagani <umair.chagani@gmail.com>
2018-04-19 11:23:28 -0400

    add selenium as dependency

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
 
 setup(name='snips_helper',
-      version='0.4',
+      version='0.5',
       description='Snips.ai Helper',
       url='http://github.com/uchagani/snips_helper',
       author='Umair Chagani',
       author_email='umair.chagani@gmail.com',
       license='MIT',
       packages=['snips_helper'],
+      install_requires=['selenium'],
       zip_safe=False)
